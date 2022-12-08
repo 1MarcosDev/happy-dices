1c3c97b1fd35a19892175985f1854ce9bd859859
Marcos Antônio <90413458+1MarcosDev@users.noreply.github.com>
2022-12-08 10:03:56 -0300

    Suporte a soma dos dados
    Além do suporte a soma, foi corrigido alguns erros de designer pattern

--- roll.nim
+++ roll.nim
@@ -2,33 +2,59 @@ import std/strutils, random
 
 randomize()
 
+proc formatDice(input: string): string =
+
+  var fomatedDice: string = input 
+
+  if input.contains('+'):
+    let
+      sumPos: int = fomatedDice.find('+')
+      last: int = fomatedDice.len() - 1
+    
+    fomatedDice.delete(sumPos .. last)
+  
+  result = fomatedDice
+
+proc readSum*(input: string): int =
+
+  var sum: string = input
+
+  result = 0
+
+  if input.contains('+'):
+    let sumPos: int = sum.find('+')
+
+    sum.delete(0 .. sumPos)
+
+    result = sum.parseInt()
+
 proc readQuant(input: string): int =
 
   var quant: string = input
 
   let
-    last: int = quant.len - 1
-    dPos: int = quant.find("d")
+    last: int = quant.len() - 1
+    dPos: int = quant.find('d')
 
   quant.delete(dPos .. last)
 
-  return quant.parseInt
+  return quant.parseInt()
 
 proc readDice(input: string): int =
 
-  var dice: string = input
+  var dice: string = input.formatDice()
 
-  let dPos: int = dice.find("d")
+  let dPos: int = dice.find('d')
 
   dice.delete(0 .. dPos)
 
-  return dice.parseInt
+  return dice.parseInt()
 
 proc roll*(input: string): seq[int] =
-
+  
   let
-    quant: int = input.readQuant
-    dice:  int = input.readDice
+    quant: int = input.readQuant()
+    dice:  int = input.readDice()
 
   var dices: seq[int] = @[]
 
